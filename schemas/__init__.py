4d8fb76bdda694816825db6d3400027e53a8f735
eedu7 <mueedahmad067@gmail.com>
2024-06-30 21:36:17 +0500

    FEAT: Added Token and GoogleToken

---
+++ schemas/__init__.py
@@ -0,0 +1,7 @@
+from .token import Token
+from .google import GoogleToken
+
+__all__ = [
+    "Token",
+    "GoogleToken",
+]
--- schemas/token.py
+++ schemas/token.py
@@ -1,7 +1,9 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class Token(BaseModel):
     access_token: str
     refresh_token: str
-    expires_in: int
+    expires_in: Optional[int]
